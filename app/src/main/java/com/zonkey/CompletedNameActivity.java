fceb9e37c0efb67be20f46e15dcdb84fd6772e61
Zonkey Master <nickalasb@gmail.com>
2016-03-07 19:27:12 -0800

    Booya!! Figured out how to delay the 2nd Textview from showing until the first textview slides in (used a handler and runnable) Also figured out how to make sure the animation ends being visible

--- app/src/main/java/com/zonkey/CompletedNameActivity.java
+++ app/src/main/java/com/zonkey/CompletedNameActivity.java
@@ -3,6 +3,7 @@ package com.zonkey;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.widget.TextView;
@@ -27,7 +28,7 @@ public class CompletedNameActivity extends AppCompatActivity {
         //before animate this sets the view to the left of the screen by the width of the current screen
         completedNameTextView1.setTranslationX(-displayMetrics.widthPixels);
 
-        //delays the animation
+        //delays the first animation
         completedNameTextView1.post(new Runnable() {
             @Override
             public void run() {
@@ -36,30 +37,38 @@ public class CompletedNameActivity extends AppCompatActivity {
             }
         });
 
-        Intent completedNameIntent = getIntent();
-        if (completedNameIntent.hasExtra("finalStripperName")) {
-            completedNameTextView2.setText(getIntent().getStringExtra("finalStripperName") + "!!!");
-        }
 
-        //This starts the view as not visible
-        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0f,1f);
+        //this handler delays the 2nd textview from showing on the screen
+        final Handler textView2Handler = new Handler();
+        Runnable textView2Runnable = new Runnable() {
+            public void run() {
+                //this gets the extra of the previous activity to put the actual stripper name on the screen
+                Intent completedNameIntent = getIntent();
+                if (completedNameIntent.hasExtra("finalStripperName")) {
+                    completedNameTextView2.setText(getIntent().getStringExtra("finalStripperName") + "!!!");
+                }
+            }
+        };
+        textView2Handler.postDelayed(textView2Runnable, 1250);
+
+
+        //This handles the animation of the 2nd textview
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(1f, 0f);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 completedNameTextView2.setAlpha((Float) animation.getAnimatedValue());
             }
         });
-
-        completedNameTextView2.post(new Runnable() {
-            @Override
-            public void run() {
-                completedNameTextView1.animate().translationX(0f).setDuration(1000).start();
-
-            }
-        });
-        valueAnimator.setDuration(250).setRepeatCount(9);
+        valueAnimator.setDuration(250).setRepeatCount(5);
+        valueAnimator.setStartDelay(1250);
         valueAnimator.start();
+        //This line is important because it defines that the animation ends on a floatValue of 1f(visible)
+        valueAnimator.setFloatValues(1f);
+
+
     }
+
 
 }
 
